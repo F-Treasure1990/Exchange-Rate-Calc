3f45d15e573ccbc163d0e0d4b1056536daae64fe
Fenton <Fentontreasure@gmail.com>
2021-03-05 10:54:01 +0000

    Simplified all the javascript, credit to Brad Traversy

--- Utilities.js
+++ Utilities.js
@@ -1,24 +1,16 @@
-export const countryToo = document.querySelector('.countryToo')
 export const [selectCountryDropDownMenuOne, selectCountryDropDownMenuTwo] = document.
   querySelectorAll('#selectCountry')
-export const countryFrom = document.querySelector('.countryFrom')
-export const priceConverted = document.querySelector('.priceConverted')
-export const unitNumber = document.querySelector('.unitNumber')
-export const unitInput1 = document.querySelector('.unitInput1')
-export const unitInput2 = document.querySelector('.unitInput2')
+export const unitInput = document.querySelector('.unitInput1')
+export const unitOutput = document.querySelector('.unitInput2')
 
-function fontBlack(array) {
-  array.forEach((arr)=>
-  arr.style.color = 'black')
-}
-
-fontBlack([countryToo, countryFrom])
 
 export async function fetchRates(url) {
   const response = await fetch(url)
   const rates = await response.json()
   return rates
 }
+
+
 
 export const countryDropDown = (obj, parent) => {
   for (const rate in obj) {
@@ -30,19 +22,6 @@ export const countryDropDown = (obj, parent) => {
 }
 
 
-export const dropDownMenuChange = (element, element2) => {
-  element.addEventListener('change', async () => {
-    let rateObj = await fetchRates(`https://api.exchangeratesapi.io/latest?base=${element.value}`)
-    countryFrom.innerHTML = selectCountryDropDownMenuOne.value;
 
-    for (const rate of Object.entries(rateObj.rates)) {
-      const [country, covRate] = rate
-      if (element2.value === country){
-          priceConverted.innerText = `${covRate.toFixed(4)}`
-          countryToo.innerText = selectCountryDropDownMenuTwo.value
-      }
-    }
-  })
-}
 
 
--- script.js
+++ script.js
@@ -1,20 +1,35 @@
-import { fetchRates, countryDropDown, dropDownMenuChange, selectCountryDropDownMenuOne, selectCountryDropDownMenuTwo,countryFrom, unitNumber, unitInput1} from './Utilities.js'
+import { fetchRates, countryDropDown, selectCountryDropDownMenuOne, selectCountryDropDownMenuTwo } from './Utilities.js'
 const weatherAppUrl = 'https://api.exchangeratesapi.io/latest'
+ const unitInput = document.querySelector('.unitInput1')
 
 document.addEventListener('DOMContentLoaded', async () => {
   let Allrates = await fetchRates(weatherAppUrl)
   countryDropDown(Allrates.rates, selectCountryDropDownMenuOne)
   countryDropDown(Allrates.rates, selectCountryDropDownMenuTwo)
-  countryFrom.innerText = selectCountryDropDownMenuOne.value
-  unitNumber.innerText = unitInput1.value
+  calc()
 })
 
-dropDownMenuChange(selectCountryDropDownMenuTwo, selectCountryDropDownMenuOne)
 
-dropDownMenuChange(selectCountryDropDownMenuOne, selectCountryDropDownMenuTwo)
+selectCountryDropDownMenuOne.addEventListener('change', () => calc())
+selectCountryDropDownMenuTwo.addEventListener('change', () => calc())
+unitInput.addEventListener('change', () => calc())
 
 
+async function calc() {
+  const exchangeRateCalc = document.querySelector('.exchangeRateCalc')
+ 
+  const unitOutput = document.querySelector('.unitInput2')
+  const currency_one = selectCountryDropDownMenuOne.value;
+  const currency_two = selectCountryDropDownMenuTwo.value;
 
+  let rates = await fetchRates(`https://api.exchangeratesapi.io/latest?base=${currency_one}`)
+  let rate = rates.rates[currency_two]
+  exchangeRateCalc.innerText = `1 ${currency_one} = ${rate} ${currency_two}`
+
+  unitOutput.value = (unitInput.value * rate).toFixed(3)
+
+
+}
 
 
 
